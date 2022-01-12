10e85bc79a37f349d21628391f0b6bea51bc7000
inhyeeeok <nangmanvv@hanmail.net>
2022-01-12 15:23:37 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
-import Main from './views/main/Main';
-import Board from './views/community/Board';
-import Intro from './views/intro/Intro';
-import Header from './components/headers/Header';
-import Footer from './components/footers/Footer';
+// import Main from './views/main/Main';
+// import Board from './views/community/Board';
+// import Intro from './views/intro/Intro';
+// import Header from './components/headers/Header'
+// import Footer from './components/footers/Footer';
 
 import './assets/style/style.css';
 import './assets/style/custom.css';
@@ -16,14 +16,14 @@ import './assets/style/custom.css';
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <Header />
+      {/* <Header /> */}
         <Routes>
-          <Route path='/' element={<Main />}></Route>
+          {/* <Route path='/' element={<Main />}></Route>
           <Route path='/board' element={<Board />}></Route>
           <Route path='/intro' element={<Intro />}></Route>
-          <Route path="/*" element={<Main />} />
+          <Route path="/*" element={<Main />} /> */}
         </Routes>
-      <Footer />
+      {/* <Footer /> */}
       {/* <Navigate from='*' to='/' /> */}
     </React.StrictMode>
   </BrowserRouter>,
