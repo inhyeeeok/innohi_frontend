da3e81cb172e13275f6ac7e09c85902a3507193c
inhyeeeok <nangmanvv@hanmail.net>
2022-01-21 11:05:57 +0900

    스타트업 아카이브 동적 렌더링 추가 close #6

--- src/views/program/StartupArchiveElements.js
+++ src/views/program/StartupArchiveElements.js
@@ -10,97 +10,13 @@ export const BlogElements =
 
             <div className="row">
                 <div id="entry" className="col-lg-8 entries">
-                    <article className="entry">
-
-                        <div className="entry-img">
-                            <img src="" alt="" className="img-fluid"></img>
-                        </div>
-                        <h2 className="entry-title">
-                            <a href="startupDetail.html">비접촉 생체 신호 모니터링 서비스. <span style={{ color: 'rgb(253, 142, 77)' }}>Smedo</span></a>
-                        </h2>
-                        <div className="entry-meta">
-                            <ul>
-                                <li className="d-flex align-items-center"><i className="bi bi-person"></i> <a href="startupDetail.html">30인</a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-clock"></i> <a href="startupDetail.html"><time datetime="2020-01-01">2021 DFS</time></a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-chat-dots"></i> <a href="startupDetail.html">의료</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="entry-content">
-                            <p>
-                                심박수, 심박변이도, 심장소리, 호흡수, 호흡의 진폭을 측정, 혈압측정은 개발중. ECG(접촉 없이 24시간 동안 모니터링 담당). 7m 반경 내 측정 가능(어둠 속, 의류나 가구가 있어도). 유아돌연사 방지 모니터링. 중증노인 24시간 모니터링. 코로나 증상자 관리. 의자에 앉아 있는 사람의 생체 신호 추적 등 정보를 제공합니다.
-                            </p>
-                        </div>
-
-                    </article>
-
-                    <article className="entry">
-
-                        <div className="entry-img">
-                            <img src="" alt="" className="img-fluid"></img>
-                        </div>
-
-                        <h2 className="entry-title">
-                            <a href="blog-single.html">원격 환자 모니터링 플랫폼. HealthBeats</a>
-                        </h2>
-
-                        <div className="entry-meta">
-                            <ul>
-                                <li className="d-flex align-items-center"><i className="bi bi-person"></i> <a href="blog-single.html">10인</a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">2021 DFS</time></a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-chat-dots"></i> <a href="blog-single.html">의료</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="entry-content">
-                            <p>
-                                iHealth Labs(실리콘밸리 업체)의 IoT의료 기구 유통업체, IoT 의료기구와 연동된 앱에 저장되는 신체 정보를 관리해 주는 서비스 등을 제공. 3일내 빠른 배포와 사용자 편리성 강조, 의료종사자를 위한 페이퍼리스 지원, 환자당 월정액 모델을 지원합니다.
-                            </p>
-                        </div>
-
-                    </article>
-
-                    <article className="entry">
-
-                        <div className="entry-img">
-                            <img src="" alt="" className="img-fluid"></img>
-                        </div>
-
-                        <h2 className="entry-title">
-                            <a href="blog-single.html"><span style={{color : 'rgb(253, 142, 77'}}>Smedo.</span><br />비접촉 생체 신호 모니터링 서비스. </a>
-                        </h2>
-
-                        <div className="entry-meta">
-                            <ul>
-                                <li className="d-flex align-items-center"><i className="bi bi-person"></i> <a href="blog-single.html">30인</a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">2021 DFS</time></a></li>
-                                <li className="d-flex align-items-center"><i className="bi bi-chat-dots"></i> <a href="blog-single.html">의료</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="entry-content">
-                            <p>
-                                심박수, 심박변이도, 심장소리, 호흡수, 호흡의 진폭을 측정, 혈압측정은 개발중. ECG(접촉 없이 24시간 동안 모니터링 담당). 7m 반경 내 측정 가능(어둠 속, 의류나 가구가 있어도). 유아돌연사 방지 모니터링. 중증노인 24시간 모니터링. 코로나 증상자 관리. 의자에 앉아 있는 사람의 생체 신호 추적 등 정보를 제공합니다.
-                            </p>
-                        </div>
-
-                    </article>
-
-                    <div className="blog-pagination">
-                        <ul className="justify-content-center">
-                            <li><a href="/">1</a></li>
-                            <li className="active"><a href="/">2</a></li>
-                            <li><a href="/">3</a></li>
-                        </ul>
-                    </div>
 
                 </div>
 
                 <div className="col-lg-4">
-
                     <div className="sidebar">
-
                         <h3 className="sidebar-title">검색</h3>
+
                         <div className="sidebar-item search-form">
                             <form action="">
                                 <input type="text"></input>
@@ -143,4 +59,49 @@ export const BlogElements =
         </div>
     </section>
 
+    ;
+
+export const ArticleElements = (Data) => {
+    return (
+        Data.map((v, i) => {
+            return (
+                <article className="entry">
+
+                    <div className="entry-img">
+                        <img src="" alt="" className="img-fluid"></img>
+                    </div>
+
+                    <h2 className="entry-title">
+                        <a href={'/startupDetail/'+ v.S_NO}>{v.S_MAIN_SERVICE}. <span style={{ color: 'rgb(253, 142, 77)' }}>{v.S_NAME}</span></a>
+                    </h2>
+
+                    <div className="entry-meta">
+                        <ul>
+                            <li className="d-flex align-items-center"><i className="bi bi-person"></i> <div>{v.S_EMPLOYEE_NUM}</div></li>
+                            <li className="d-flex align-items-center"><i className="bi bi-clock"></i> <div>{v.S_RESEARCHED_TIME}</div></li>
+                            <li className="d-flex align-items-center"><i className="bi bi-chat-dots"></i> <div>{v.S_TECH_TYPE}</div></li>
+                        </ul>
+                    </div>
+
+                    <div className="entry-content">
+                        <p>{v.S_SERVICE_DETL}</p>
+                    </div>
+
+                </article>
+            )
+        })
+    )
+} 
+
+export const page =
+    <div className="blog-pagination">
+        <ul className="justify-content-center">
+            <li><a href="/">1</a></li>
+            <li className="active"><a href="/">2</a></li>
+            <li><a href="/">3</a></li>
+        </ul>
+    </div>
+    ;
+
+export const sd = '2'
     ;
