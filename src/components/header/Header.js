ce2cb2e45877858662c4cfd3f874f592f626a8b3
inhyeeeok <nangmanvv@hanmail.net>
2022-01-25 15:28:02 +0900

    메뉴 및 경로 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,7 +14,7 @@ const headerElements =
       </h1>
       <nav id='navbar' className='navbar'>
         <ul>
-        <li className='nav-link scrollto'><a href='/'><div>HOME</div></a></li>
+        {/* <li className='nav-link scrollto'><a href='/'><div>HOME</div></a></li> */}
           <li className='dropdown'><a href='/intro'><div>소개<i className='bi bi-chevron-down'></i></div></a>
             <ul>
               <li><div>innoHI 소개</div></li>
@@ -23,25 +23,25 @@ const headerElements =
               <li><div>자주묻는 질문</div></li>
             </ul>
           </li>
-          <li className='dropdown'><a href='/dfs'><div>프로그램<i className='bi bi-chevron-down'></i></div></a>
+          <li className='dropdown'><a href='/program/startup'><div>프로그램<i className='bi bi-chevron-down'></i></div></a>
             <ul>
+              <li><a href='/program/startup'><div>스타트업 소싱</div></a></li>
+              <li><a href='/program/external'><div>대외 사업</div></a></li>
+              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLSdHnXT1VGX3OWoH8K1slTlkIWtLNIpNShk5k-8fBI-CxO6yow/viewform'><div>프로그램 참여</div></a></li>
               <li><a href='/dfs'><div>DFS(Deal Flow Session)</div></a></li>
-              <li><a href='/startup'><div>스타트업 아카이브</div></a></li>
-              {/* <li className='dropdown'><div>DFS(Deal Flow Session)<i className='bi bi-chevron-down'></i></div>
-      <ul>
-        <li><div>DFS 란?</div></li>
-        <li><div>DFS 적용 과정</div></li>
-        <li><div>산업별 적용 사례</div></li>
-      </ul>
-    </li> */}
             </ul>
           </li>
-          <li className='nav-link scrollto'><a href='/event'><div>이벤트</div></a></li>
-          <li className='dropdown'><a href='/board'><div>커뮤니티<i className='bi bi-chevron-down'></i></div></a>
+          <li className='dropdown'><a href='/event/oi'><div>이벤트<i className='bi bi-chevron-down'></i></div></a>
             <ul>
-              <li><a href='/board'><div>OI 뉴스레터</div></a></li>
+              <li><a href='/event/oi'><div>OI 이벤트</div></a></li>
+              <li><a href='/event/contest'><div>공모전</div></a></li>
+            </ul>
+          </li>
+          <li className='dropdown'><a href='/community/board'><div>커뮤니티<i className='bi bi-chevron-down'></i></div></a>
+            <ul>
+              <li><a href='/community/board'><div>OI 뉴스레터</div></a></li>
+              <li><a href='/community/startup'><div>스타트업 찾기</div></a></li>
               <li><a href='https://docs.google.com/forms/d/e/1FAIpQLScB_r-XIOR0H9I6HxdrwIE4MFqWmP7ho1lOsT_ijsAOmCcMlQ/viewform'><div>문의 및 제안하기</div></a></li>
-              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLSdHnXT1VGX3OWoH8K1slTlkIWtLNIpNShk5k-8fBI-CxO6yow/viewform'><div>스타트업 소싱 요청</div></a></li>
             </ul>
           </li>
           <li><a href='/council'><div className='getstarted'>협의체공간</div></a></li>
