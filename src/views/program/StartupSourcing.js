62ac27e26f4b22582624e7b8486e1a9660e06f28
inhyeeeok <nangmanvv@hanmail.net>
2022-01-25 15:30:49 +0900

    프로그램 스타트업 소싱 목업 추가

---
+++ src/views/program/StartupSourcing.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import * as main from '../main/MainElements';
+import * as dfs from '../program/DfsElements'
+
+const StartupSourcing = () => {
+
+    useEffect(() => {
+
+    });
+
+    return (
+        <>
+            {main.heroElements}
+            {main.refElements}
+            {main.heroElements}
+            {main.refElements}
+            {main.heroElements}
+            {dfs.ServiceElements}
+        </>
+    );
+
+}
+
+export default StartupSourcing;
