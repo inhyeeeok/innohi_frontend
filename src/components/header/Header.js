e39b87847c6a669eee19a66b7a38c2cc0623478b
inhyeeeok <nangmanvv@hanmail.net>
2022-01-25 17:23:40 +0900

    메뉴 이름 변경

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -18,22 +18,22 @@ const headerElements =
           <li className='dropdown'><a href='/intro'><div>소개<i className='bi bi-chevron-down'></i></div></a>
             <ul>
               <li><div>innoHI 소개</div></li>
-              <li><div>발자취</div></li>
-              <li><div>스타트업 생태계</div></li>
-              <li><div>자주묻는 질문</div></li>
+              <li><div>Vision</div></li>
+              <li><div>이노하이 발자취</div></li>
+              {/* <li><div>자주묻는 질문</div></li> */}
             </ul>
           </li>
           <li className='dropdown'><a href='/program/startup'><div>프로그램<i className='bi bi-chevron-down'></i></div></a>
             <ul>
-              <li><a href='/program/startup'><div>스타트업 소싱</div></a></li>
-              <li><a href='/program/external'><div>대외 사업</div></a></li>
-              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLSdHnXT1VGX3OWoH8K1slTlkIWtLNIpNShk5k-8fBI-CxO6yow/viewform'><div>프로그램 참여</div></a></li>
-              <li><a href='/dfs'><div>DFS(Deal Flow Session)</div></a></li>
+              <li><a href='/program/startup'><div>스타트업 소싱<br />(한진그룹>스타트업)</div></a></li>
+              <li><a href='/program/external'><div>대외 협력<br />(한진그룹+α>스타트업)</div></a></li>
+              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLSdHnXT1VGX3OWoH8K1slTlkIWtLNIpNShk5k-8fBI-CxO6yow/viewform'><div>협업 제안<br />(스타트업>한진그룹)</div></a></li>
+              {/* <li><a href='/dfs'><div>DFS(Deal Flow Session)</div></a></li> */}
             </ul>
           </li>
           <li className='dropdown'><a href='/event/oi'><div>이벤트<i className='bi bi-chevron-down'></i></div></a>
             <ul>
-              <li><a href='/event/oi'><div>OI 이벤트</div></a></li>
+              <li><a href='/event/oi'><div>세미나 및 컨퍼런스</div></a></li>
               <li><a href='/event/contest'><div>공모전</div></a></li>
             </ul>
           </li>
