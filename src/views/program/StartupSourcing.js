161f31d4d134c8eec2e7d54ed91ed38d3361e908
inhyeeeok <nangmanvv@hanmail.net>
2022-01-25 16:51:17 +0900

    안쓰는 import 제거

--- src/views/program/StartupSourcing.js
+++ src/views/program/StartupSourcing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import * as main from '../main/MainElements';
 import * as dfs from '../program/DfsElements'
 
