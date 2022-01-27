5b33ad85017de076c1f06d528ee2456fa223965d
inhyeeeok <nangmanvv@hanmail.net>
2022-01-27 15:57:03 +0900

    portfolio-details-slider swiper 추가

--- src/components/swipers/Swiper.js
+++ src/components/swipers/Swiper.js
@@ -67,6 +67,23 @@ const SwiperComponents = () => {
     }
   });
 
+  /**
+   * Portfolio details slider
+   */
+   new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
 }
 
 export default SwiperComponents;
