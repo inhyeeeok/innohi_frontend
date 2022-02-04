213aab016dd33155307d97a3b2f34bc9062d69d5
inhyeeeok <nangmanvv@hanmail.net>
2022-02-04 15:10:39 +0900

    문구 및 공란 추가

--- src/views/contact/Member.js
+++ src/views/contact/Member.js
@@ -3,32 +3,34 @@ import React, { useEffect } from 'react';
 const Member = () => {
 
     const serviceElements =
-    <>
-     <section id="services" className="services section-bg" style={{ marginTop: '61px' }}>
-            <div className="container" data-aos="fade-up">
+        <>
+            <section id="services" className="services section-bg" style={{ marginTop: '61px' }}>
+                <div className="container" data-aos="fade-up">
 
-                <div className="section-title">
+                    <div className="section-title">
                         <h2>구성원 소개</h2>
                         <p>inno H.I. 프로그램 및 스타트업 관련 궁금하신 사항은
                             문의 주시면  담당자가 연락드리도록 하겠습니다.</p>
-                </div>
+                    </div>
 
-                <div className="row">
-                    <div className="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                        <div className="icon-box">
-                            <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                    <div className="row">
+                        <div className="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                            <div className="icon-box">
+                                <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                                <p><br /></p>
                                 <h4>김은희</h4>
                                 <p>디지털 아키텍트</p>
                                 <p>전략사업부 디지털솔루션팀 솔루션사업그룹장</p>
                                 <p>HIST C&D 엔지니어</p>
                                 <p>AWS S.A</p>
                                 <h5>ehkim@hist.co.kr</h5>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-                        <div className="icon-box">
-                        <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                        <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                            <div className="icon-box">
+                                <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                                <p><br /></p>
                                 <h4>강준수</h4>
                                 <p>디지털 아키텍트</p>
                                 <p>전략사업부 디지털솔루션팀 대리</p>
@@ -36,25 +38,27 @@ const Member = () => {
                                 <p><br /></p>
                                 <p><br /></p>
                                 <h5>js.kang@hist.co.kr</h5>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-                        <div className="icon-box">
-                        <img src={require('../../assets/img/contact/member/test.jpg').default} className="img-fluid" alt=""></img>
+                        <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+                            <div className="icon-box">
+                                <img src={require('../../assets/img/contact/member/test.jpg').default} className="img-fluid" alt=""></img>
+                                <p><br /></p>
                                 <h4>권인혁</h4>
                                 <p>디지털 아키텍트</p>
                                 <p>전략사업부 디지털솔루션팀 대리</p>
-                                <p><br /></p>
+                                <p>개발 및 PoC 수행 담당</p>
                                 <p><br /></p>
                                 <p><br /></p>
                                 <h5>ikwon@hist.co.kr</h5>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-                        <div className="icon-box">
-                        <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                        <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+                            <div className="icon-box">
+                                <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                                <p><br /></p>
                                 <h4>한태준</h4>
                                 <p>디지털 아키텍트</p>
                                 <p>전략사업부 디지털솔루션팀 사원</p>
@@ -62,13 +66,13 @@ const Member = () => {
                                 <p><br /></p>
                                 <p><br /></p>
                                 <h5>than@hist.co.kr</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </>
-    ;
+            </section>
+        </>
+        ;
 
     useEffect(() => {
 
@@ -76,7 +80,7 @@ const Member = () => {
 
     return (
         <>
-        {serviceElements}
+            {serviceElements}
         </>
     );
 
