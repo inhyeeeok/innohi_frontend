e7bc2eb7365cfd4a22b5370e25059c67193cae0d
inhyeeeok <nangmanvv@hanmail.net>
2022-02-04 15:11:45 +0900

    사진 크기 조정 및 사진 센터

--- src/views/main/MainElements.js
+++ src/views/main/MainElements.js
@@ -25,12 +25,12 @@ export const refElements =
     <section id='hero' className='d-flex align-items-center' style={{ backgroundColor: '#f3f5fa' }}>
       <div className='container'>
         <div className='row'>
-        <div className='col-lg-6  hero-img' data-aos="zoom-in" data-aos-delay="200">
+          <div className='col-lg-6  hero-img' data-aos="zoom-in" data-aos-delay="200">
             <img src={require('../../assets/img/tour_DFS.jpg').default} className='img-fluid animated' alt=''></img>
           </div>
           <div className='col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 ' data-aos="fade-up" data-aos-delay="200">
-            <h1><span style={{ color: '#37517e'}}>The HIST group company DFS was held</span></h1>
-            <h2><span style={{ color: '#37517e'}}>디지털솔루션팀 솔루션사업그룹에서는 11월 17일 본사 5층 대회의실에서 ‘여행 플랫폼과 에듀테크 분야’ 스타트업 발굴을 주제로 PnP DFS(Deal Flow Session)를 개최했습니다.</span></h2>
+            <h1><span style={{ color: '#37517e' }}>The HIST group company DFS was held</span></h1>
+            <h2><span style={{ color: '#37517e' }}>디지털솔루션팀 솔루션사업그룹에서는 11월 17일 본사 5층 대회의실에서 ‘여행 플랫폼과 에듀테크 분야’ 스타트업 발굴을 주제로 PnP DFS(Deal Flow Session)를 개최했습니다.</span></h2>
             <div className='d-flex justify-content-center justify-content-lg-end'>
               <div href='' className='btn-get-started scrollto'>자세히 알아보기</div>
             </div>
@@ -96,7 +96,7 @@ export const clientsElements =
 
       <div className="clients-slider swiper">
         <div className="swiper-wrapper align-items-center">
-          <div className="swiper-slide"><img src={require('../../assets/img/insight/contest/만반잘부.jpg').default} className="img-fluid" style={{ width: "320px", height: "200px"}} alt=""></img></div>
+          <div className="swiper-slide"><img src={require('../../assets/img/insight/contest/만반잘부.jpg').default} className="img-fluid" style={{ width: "320px", height: "200px" }} alt=""></img></div>
           <div className="swiper-slide"><img src={require('../../assets/img/insight/contest/PnP_Web3_and_Big_Data_in_Healthcare.png').default} className="img-fluid" style={{ width: "320px", height: "200px" }} alt=""></img></div>
           <div className="swiper-slide"><img src={require('../../assets/img/insight/contest/APAC SUMMIT 2021.png').default} className="img-fluid" style={{ width: "320px", height: "200px" }} alt=""></img></div>
         </div>
@@ -107,52 +107,52 @@ export const clientsElements =
   </section>
   ;
 
-  export const joinNewsletterElements =
+export const joinNewsletterElements =
 
-    <div id='footer'>
-      <div className='footer-newsletter'>
-        <div className='container'>
+  <div id='footer'>
+    <div className='footer-newsletter'>
+      <div className='container'>
         <div className="col-lg-12">
-            <h4>관심 분야의 행사소식을 가장 먼저 접하세요.</h4>
-            <p>기술혁신의 인사이트를 얻을 수 있는 innoH.I. 정기레터를 무료로 구독 할 수 있으며 비지니스에 부합하는 컨설팅, 교육프로그램을 운영할 수 있습니다.</p>
-              <a href="/board" className="btn-get-started scrollto"><strong>지난 뉴스레터 보기</strong></a>
-          </div>
+          <h4>관심 분야의 행사소식을 가장 먼저 접하세요.</h4>
+          <p>기술혁신의 인사이트를 얻을 수 있는 innoH.I. 정기레터를 무료로 구독 할 수 있으며 비지니스에 부합하는 컨설팅, 교육프로그램을 운영할 수 있습니다.</p>
+          <a href="/board" className="btn-get-started scrollto"><strong>지난 뉴스레터 보기</strong></a>
         </div>
       </div>
     </div>
+  </div>
   ;
 
-  export const serviceElements =
-    <>
-      <section id="services" className="services section-bg">
-        <div className="container" data-aos="fade-up">
+export const serviceElements =
+  <>
+    <section id="services" className="services section-bg">
+      <div className="container" data-aos="fade-up">
 
-          <div className="row">
-            <div className="col-xl-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-              <div className="icon-box">
-                <img src={require('../../assets/img/main/intro/innoHI소개_1.png').default} className="img-fluid" alt=""></img>
-                <h4 style={{ textAlign:'center', fontSize:'2vw', fontWeight:'bold' }}>미래성장동력 확보</h4>
-                <p style={{ textAlign:'center', fontSize:'1vw' }}>최신 산업동향과 생태계 분석을 통해 아이디어를 도출하고 미래 성장동력을 확보합니다</p>
-              </div>
+        <div className="row">
+          <div className="col-xl-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div className="icon-box" style={{ textAlign: 'center' }}>
+              <img src={require('../../assets/img/main/intro/innoHI소개_1.png').default} className="img-fluid" style={{ width: '70%' }} alt=""></img>
+              <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>미래성장동력 확보</h4>
+              <p style={{ textAlign: 'center', fontSize: '1vw' }}>최신 산업동향과 생태계 분석을 통해 아이디어를 도출하고 미래 성장동력을 확보합니다</p>
             </div>
+          </div>
 
-            <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-              <div className="icon-box">
-                <img src={require('../../assets/img/main/intro/innoHI소개_2.png').default} className="img-fluid" alt=""></img>
-                <h4 style={{ textAlign:'center', fontSize:'2vw', fontWeight:'bold' }}>차세대 기술 선도</h4>
-                <p style={{ textAlign:'center', fontSize:'1vw' }}>디지털전환에 대비하여 최신 ICT 기술을 확보하고 차세대 기술을 선도합니다</p>
-              </div>
+          <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div className="icon-box" style={{ textAlign: 'center' }}>
+              <img src={require('../../assets/img/main/intro/innoHI소개_2.png').default} className="img-fluid" style={{ width: '70%' }} alt=""></img>
+              <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>차세대 기술 선도</h4>
+              <p style={{ textAlign: 'center', fontSize: '1vw' }}>디지털전환에 대비하여 최신 ICT 기술을 확보하고 차세대 기술을 선도합니다</p>
             </div>
+          </div>
 
-            <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-              <div className="icon-box">
-                <img src={require('../../assets/img/main/intro/innoHI소개_3.png').default} className="img-fluid" alt=""></img>
-                <h4 style={{ textAlign:'center', fontSize:'2vw', fontWeight:'bold' }}>글로벌 혁신</h4>
-                <p style={{ textAlign:'center', fontSize:'1vw' }}>글로벌 파트너십을 통해 변화와 혁신하며 신규시장진출의 기반을 마련합니다</p>
-              </div>
+          <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div className="icon-box" style={{ textAlign: 'center' }}>
+              <img src={require('../../assets/img/main/intro/innoHI소개_3.png').default} className="img-fluid" style={{ width: '70%' }} alt=""></img>
+              <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>글로벌 혁신</h4>
+              <p style={{ textAlign: 'center', fontSize: '1vw' }}>글로벌 파트너십을 통해 변화와 혁신하며 신규시장진출의 기반을 마련합니다</p>
             </div>
           </div>
         </div>
-      </section>
-    </>
-    ;
+      </div>
+    </section>
+  </>
+  ;
--- src/views/program/Suggestion.js
+++ src/views/program/Suggestion.js
@@ -14,41 +14,46 @@ const Suggestion = () => {
 
             <div className="row">
               <div className="col-xl-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                <div className="icon-box">
-                  <img src={require('../../assets/img/program/contact_1.png').default} className="img-fluid" alt=""></img>
+                <div className="icon-box" style={{ textAlign : 'center'}}>
+                  <img src={require('../../assets/img/program/contact_1.png').default} className="img-fluid" style={{ width: "80%"}} alt=""></img>
                   <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>다양한 산업분야 진출기반 마련</h4>
                   <p style={{ textAlign: 'center', fontSize: '1vw' }}>물류, 항공, IT, 의료 등  다양한 분야에 진출, 활약하고 있는 한진그룹과 함께하실 수 있습니다.</p>
                 </div>
               </div>
 
               <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-                <div className="icon-box">
-                  <img src={require('../../assets/img/program/스타트업-생태계_2.png').default} className="img-fluid" alt=""></img>
+                <div className="icon-box" style={{ textAlign : 'center'}}>
+                  <img src={require('../../assets/img/program/스타트업-생태계_2.png').default} className="img-fluid" style={{ width: "80%"}} alt=""></img>
                   <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>사업 시너지 효과 창출</h4>
                   <p style={{ textAlign: 'center', fontSize: '1vw' }}> 멘토링, 사업화 연계 등 한진그룹과 협력, 교류 활동을 통해 성장하실 수 있습니다.</p>
                 </div>
               </div>
 
               <div className="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-                <div className="icon-box">
-                  <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" alt=""></img>
+                <div className="icon-box" style={{ textAlign : 'center'}}>
+                  <img src={require('../../assets/img/program/DFS_4.png').default} className="img-fluid" style={{ width: "80%"}} alt=""></img>
                   <h4 style={{ textAlign: 'center', fontSize: '2vw', fontWeight: 'bold' }}>ICT기술 전문성 바탕의 PoC 검증 제공</h4>
                   <p style={{ textAlign: 'center', fontSize: '1vw' }}> ICT분야 전문성을 바탕으로 제품/서비스의 우수성을 검증하며 성공적 협업을 달성합니다.</p>
                 </div>
               </div>
             </div>
 
+            <br /> <br />
+            <div className='d-flex justify-content-center justify-content-start'>
+              <a href='https://forms.gle/bm1Cniym3XFxSuzD9'><div className='btn-get-started scrollto'>협업 제안하기</div></a>
+            </div>
+
           </div>
         </section>
 
-        <section id="services" className="services section">
+        {/* <section id="services" className="services section">
           <div className="container" data-aos="fade-up">
             <iframe
               src='https://forms.gle/bm1Cniym3XFxSuzD9'
               style={{ height: '1000px', width: '100%' }} title="oi-newsletter">
             </iframe>
           </div>
-        </section>
+        </section> */}
     </>
     ;
 
