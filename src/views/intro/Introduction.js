011bd1aff7af52fee74844e0d6a1e65c4406f7a0
inhyeeeok <nangmanvv@hanmail.net>
2022-02-04 15:11:30 +0900

    소개 부분 분기 처리

---
+++ src/views/intro/Introduction.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+import * as intro from './IntroElements';
+
+
+// import test from "../../components/Test";
+
+const Introduction = () => {
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <>
+            <main id="main">
+                {intro.IntroElements}
+            </main>
+        </>
+    );
+}
+
+export default Introduction;
---
+++ src/views/intro/Propel.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+import * as intro from './IntroElements';
+
+const Propel = () => {
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <>
+            <main id="main">
+                {intro.TrinityElements}
+            </main>
+        </>
+    );
+}
+
+export default Propel;
