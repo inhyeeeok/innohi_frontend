bb14168f37170175c9e3b736626c23dafc194d4c
inhyeeeok <nangmanvv@hanmail.net>
2022-02-08 17:32:58 +0900

    년도 수정 2021 -> 2022

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -38,7 +38,7 @@ const footerElements =
     </div> */}
     <div className='container footer-bottom clearfix'>
       <div className='copyright'>
-        Copyright &copy;2021 <strong><span>Hanjin Information Systems & Telecommunication</span></strong>. Co., Ltd. All rights Reserved.
+        Copyright &copy;2022 <strong><span>Hanjin Information Systems & Telecommunication</span></strong>. Co., Ltd. All rights Reserved.
       </div>
       <div className='credits'>문의하기 : SELHTDB@hist.co.kr</div>
     </div>
