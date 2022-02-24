23d51b5cbd4e35a8abd1bc72b189f5e56b8a42cd
inhyeeeok <nangmanvv@hanmail.net>
2022-02-24 10:17:12 +0900

    모달 수정본 커밋

--- src/views/council/CouncilMain.js
+++ src/views/council/CouncilMain.js
@@ -69,10 +69,45 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
     tt.changeName(user.username);
   })
 
-  return (
+  const modalElements = () => {
+    const title = 'InnoHI 사이트 리뉴얼에 따른 개인정보(이름, 이메일) 이용 안내'
+    const contents = 'test'
+    
+
+    // 안녕하세요.<br />
+    // 한진정보통신 InnoHI 운영자입니다. <br /><br />
+
+    // 저희 innohi 와 함께해주시는 여러 협의체 구성원 여러분께 진심으로 감사드리며,
+    // 사이트 리뉴얼에 따른 개인정보 이용에 관한 안내 말씀드립니다. <br /><br />
+
+    // 그동안 이노하이 웹페이지는 그룹사 계정이 웹 브라우저에 로그인되어있는 상태에서
+    // 접근이 가능한 비공개형 사이트였습니다. <br /><br />
+
+    // 이번 리뉴얼을 통해 공개 영역과 협의체 구성원 전용 비공개 영역으로
+    // 분리되어 운영될 예정이오며 <br /><br />
+    // 기존 이노하이 웹페이지를 이용하시던 그룹사 이메일 계정에 임시 비밀번호를
+    // 발급하는 방식으로 서비스 이용을 도와드릴 예정입니다.(별도 이메일 송부예정)
+    // <br /><br />
+    // 만약 이용을 원하지 않으시는 분은 아래의 연락처로 연락주시기 바랍니다.
+    // <br />
+    // <br />
+    // 이용정보 : 이름, 이메일
+    // <br />
+    // 문의처 : innoHI@hist.co.kr
+
+    return (
+      <>
+        <Modal show={show} title={title} handleClose={handleClose} handleShow={handleShow}>
+          {contents}
+        </Modal>
+      </>
+      )
+  }
+
+  return (  
     <>
 
-      <Modal show={show} handleClose={handleClose} handleShow={handleShow}>
+      <Modal show={show} title='InnoHI 사이트 리뉴얼에 따른 개인정보(이름, 이메일) 이용 안내' handleClose={handleClose} handleShow={handleShow}>
         안녕하세요.<br />
         한진정보통신 InnoHI 운영자입니다. <br /><br />
 
