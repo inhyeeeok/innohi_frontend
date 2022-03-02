51b91523ffdc60b875858a7351fcfec490299089
inhyeeeok <nangmanvv@hanmail.net>
2022-03-03 08:50:12 +0900

    불필요한 파일 제거 및 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -75,7 +75,7 @@ const headerCouncilElements =
           </li>
           <li className='dropdown' ><a id='user_name' href='/'>로그인이 필요합니다.</a>
             <ul>
-              <li id="LogOut"><a>로그아웃</a></li>
+              <li id="LogOut"><a href='/'>로그아웃</a></li>
             </ul>
           </li>
         </ul>
