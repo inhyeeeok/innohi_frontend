a198dde5db84acc52a6022b5c4f8d04eaad8ef50
inhyeeeok <nangmanvv@hanmail.net>
2022-03-10 09:17:32 +0900

    불필요한 내용 제거 및 주석 처리

--- src/views/council/CouncilMain.js
+++ src/views/council/CouncilMain.js
@@ -9,7 +9,6 @@ import * as CouncilCommon from './CouncilCommon'
 import SwiperComponents from '../../components/swipers/Swiper';
 
 import Modal from '../../components/modal/Modal'
-import { ConsoleLogger } from 'aws-amplify/node_modules/@aws-amplify/core';
 
 
 Amplify.configure(
@@ -62,9 +61,9 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
 
     console.log(sevenDay);
 
-    if (lastDay == sevenDay) {
+    if (lastDay === sevenDay) {
        localStorage.removeItem("innohi");
-    } else if(lastDay == null){
+    } else if(lastDay === null){
       localStorage.setItem("innohi", getToday());
     }
   }
@@ -78,26 +77,26 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
 
   const ModalElements = () => {
     const title = 'InnoHI 사이트 리뉴얼에 따른 개인정보(이름, 이메일) 이용 안내'
-    const contents = `안녕하세요. \n
-    한진정보통신 InnoHI 운영자입니다. \n
-    <br />
+    // const contents = `안녕하세요. \n
+    // 한진정보통신 InnoHI 운영자입니다. \n
+    // <br />
 
-    저희 innohi 와 함께해주시는 여러 협의체 구성원 여러분께 진심으로 감사드리며,
-    사이트 리뉴얼에 따른 개인정보 이용에 관한 안내 말씀드립니다. 
+    // 저희 innohi 와 함께해주시는 여러 협의체 구성원 여러분께 진심으로 감사드리며,
+    // 사이트 리뉴얼에 따른 개인정보 이용에 관한 안내 말씀드립니다. 
 
-    그동안 이노하이 웹페이지는 그룹사 계정이 웹 브라우저에 로그인되어있는 상태에서
-    접근이 가능한 비공개형 사이트였습니다. 
+    // 그동안 이노하이 웹페이지는 그룹사 계정이 웹 브라우저에 로그인되어있는 상태에서
+    // 접근이 가능한 비공개형 사이트였습니다. 
 
-    이번 리뉴얼을 통해 공개 영역과 협의체 구성원 전용 비공개 영역으로
-    분리되어 운영될 예정이오며 
-    기존 이노하이 웹페이지를 이용하시던 그룹사 이메일 계정에 임시 비밀번호를
-    발급하는 방식으로 서비스 이용을 도와드릴 예정입니다.(별도 이메일 송부예정)
+    // 이번 리뉴얼을 통해 공개 영역과 협의체 구성원 전용 비공개 영역으로
+    // 분리되어 운영될 예정이오며 
+    // 기존 이노하이 웹페이지를 이용하시던 그룹사 이메일 계정에 임시 비밀번호를
+    // 발급하는 방식으로 서비스 이용을 도와드릴 예정입니다.(별도 이메일 송부예정)
     
-    만약 이용을 원하지 않으시는 분은 아래의 연락처로 연락주시기 바랍니다.
+    // 만약 이용을 원하지 않으시는 분은 아래의 연락처로 연락주시기 바랍니다.
     
-    이용정보 : 이름, 이메일
+    // 이용정보 : 이름, 이메일
     
-    문의처 : innoHI@hist.co.kr`
+    // 문의처 : innoHI@hist.co.kr`
 
     if (localStorage.getItem("innohi") == null)
     return (
