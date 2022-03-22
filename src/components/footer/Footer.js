1e61d8ab3b6b16a264991fd5cebab3b967f757eb
inhyeeeok <nangmanvv@hanmail.net>
2022-03-22 13:15:21 +0900

    선언형 -> 함수형으로 변경

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,40 +2,11 @@ import React from 'react';
 import '../../assets/style/style.css'
 import '../../assets/style/custom.css'
 
-const footerElements =
+const Footer = () => {
+
+  const footerElements =
 
   <footer id='footer'>
-    {/* <div className='footer-top'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-3 col-md-6 footer-contact'>
-            <h3><img src={require('../../assets/img/clients/hist.png').default} className='img-fluid' style={{ width: '175px' }} alt=''></img></h3>
-            <p>한진정보통신 주식회사<br /> 대표자명: 박은호<br /> 사업자등록번호: 201-81-46253 <br />서울시 강서구 공항대로 453(등촌동)</p>
-          </div>
-          <div className='col-lg-3 col-md-6 footer-links'>
-            <h4>한진정보통신 링크</h4>
-            <ul>
-              <li><a href='https://www.hist.co.kr/'><div>회사소개</div></a></li>
-              <li><a href='https://www.hist.co.kr/contact-us/'><div>찾아오시는 길</div></a></li>
-              <li><a href='https://www.hist.co.kr/support/'><div>제휴 문의</div></a></li>
-            </ul>
-          </div>
-          <div className='col-lg-3 col-md-6 footer-links'>
-            <h4>이노하이 프로그램</h4>
-            <ul>
-              <li><a href='/contact/member'><div>문의 및 제안하기</div></a></li>
-            </ul>
-          </div>
-          <div className='col-lg-3 col-md-6 footer-links'>
-            <h4>Our Social Networks</h4>
-            <p>이노하이 관련 궁금하신 사항은 아래 연락처로 문의주시면 담당자가 답변드리겠습니다.</p>
-            <div className='social-links mt-3'>
-              <i className='bx bxl-twitter'></i><div>SELHTDB@hist.co.kr</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> */}
     <div className='container footer-bottom clearfix'>
       <div className='copyright'>
         Copyright &copy;2022 <strong><span>Hanjin Information Systems & Telecommunication</span></strong>. Co., Ltd. All rights Reserved.
@@ -45,31 +16,15 @@ const footerElements =
   </footer>
   ;
 
-class Footer extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { isToggleOn: true };
+  if(window.location.pathname.split('/')[1] === 'test') return null;
+  // if (window.location.pathname.split('/')[1] === 'login')
+  //   return null;
+  return (
+    <>
+      {footerElements}
+    </>
+  );
 
-  //   // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // handleClick() {
-  //   this.setState(prevState => ({
-  //     isToggleOn: !prevState.isToggleOn
-  //   }));
-  // }
-
-  render() {
-    // if(window.location.pathname.split('/')[1] === 'council') return null;
-    // if (window.location.pathname.split('/')[1] === 'login')
-    //   return null;
-    return (
-      <>
-        {footerElements}
-      </>
-    );
-  }
 }
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,111 +1,107 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import GlobalFunction from "../GlobalFunction";
-// import { Link } from "react-router-dom";
 
-const headerElements =
-   <header id='header' className='fixed-top header-inner-pages'>
-  {/* <header id='header' className=' header-inner-pages'> */}
-    <div className='container d-flex align-items-center'>
-      <h1 className='logo me-auto'>
-        <a href='/'>
-          <span>innohi</span>
-          <span></span>
-        </a>
-      </h1>
-      <nav id='navbar' className='navbar'>
-        <ul>
-          {/* <li className='nav-link scrollto'><a href='/'><div>HOME</div></a></li> */}
-          <li className='dropdown'><a href='/intro/introduction'>Introduce
-            {/* <i className='bi bi-chevron-down'></i> */}
+const Header = () => {
+
+  const headerElements =
+    <header id='header' className='fixed-top header-inner-pages'>
+      {/* <header id='header' className=' header-inner-pages'> */}
+      <div className='container d-flex align-items-center'>
+        <h1 className='logo me-auto'>
+          <a href='/'>
+            <span>innohi</span>
+            <span></span>
           </a>
-            {/* <ul>
+        </h1>
+        <nav id='navbar' className='navbar'>
+          <ul>
+            {/* <li className='nav-link scrollto'><a href='/'><div>HOME</div></a></li> */}
+            <li className='dropdown'><a href='/intro/introduction'>Introduce
+              {/* <i className='bi bi-chevron-down'></i> */}
+            </a>
+              {/* <ul>
               <li><a href='/intro/introduction'>이노하이란?</a></li>
               <li><a href='/intro/propel'>비전</a></li>
               <li><a href='/intro/history'>연혁</a></li>
             </ul> */}
-          </li>
-          <li className='dropdown'><a href='/program/startup'>Program<i className='bi bi-chevron-down'></i></a>
-            <ul>
-              <li><a href='/program/startup'>프로그램 소개</a></li>
-              {/* <li><a href='/program/external'>대외협력</a></li> */}
-              <li><a href='/program/suggest'>협업제안</a></li>
-            </ul>
-          </li>
-          <li className='dropdown'><a href='/insight/contest'>Insight<i className='bi bi-chevron-down'></i></a>
-            <ul>
-              <li><a href='/insight/innopick'>Inno Pick</a></li>
-              <li><a href='/insight/contest'>Event</a></li>
-            </ul>
-          </li>
-          <li className='dropdown'><a href='/contact/member'>Contact<i className='bi bi-chevron-down'></i></a>
-            <ul>
-              <li><a href='/contact/member'>구성원 소개</a></li>
-              {/* <li><a href='/contact/question'><div>문의하기</div></a></li> */}
-              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLScB_r-XIOR0H9I6HxdrwIE4MFqWmP7ho1lOsT_ijsAOmCcMlQ/viewform'>문의하기</a></li>
-            </ul>
-          </li>
-          <li><a href='/council/main' className='getstarted'>협의체공간</a>
-          </li>
-        </ul>
-        <i className='bi bi-list mobile-nav-toggle'></i>
-      </nav>
-    </div>
-  </header>;
+            </li>
+            <li className='dropdown'><a href='/program/startup'>Program<i className='bi bi-chevron-down'></i></a>
+              <ul>
+                <li><a href='/program/startup'>프로그램 소개</a></li>
+                {/* <li><a href='/program/external'>대외협력</a></li> */}
+                <li><a href='/program/suggest'>협업제안</a></li>
+              </ul>
+            </li>
+            <li className='dropdown'><a href='/insight/contest'>Insight<i className='bi bi-chevron-down'></i></a>
+              <ul>
+                <li><a href='/insight/innopick'>Inno Pick</a></li>
+                <li><a href='/insight/contest'>Event</a></li>
+              </ul>
+            </li>
+            <li className='dropdown'><a href='/contact/member'>Contact<i className='bi bi-chevron-down'></i></a>
+              <ul>
+                <li><a href='/contact/member'>구성원 소개</a></li>
+                {/* <li><a href='/contact/question'><div>문의하기</div></a></li> */}
+                <li><a href='https://docs.google.com/forms/d/e/1FAIpQLScB_r-XIOR0H9I6HxdrwIE4MFqWmP7ho1lOsT_ijsAOmCcMlQ/viewform'>문의하기</a></li>
+              </ul>
+            </li>
+            <li><a href='/council/main' className='getstarted'>협의체공간</a>
+            </li>
+          </ul>
+          <i className='bi bi-list mobile-nav-toggle'></i>
+        </nav>
+      </div>
+    </header>;
 
-const headerCouncilElements =
-//  <header id='header' className='fixed-top header-inner-pages' style={{display:'none'}}>
-  <header id='header' className='header-inner-pages' style={{display:'none'}}>
-    <div className='container d-flex align-items-center'>
-      <h1 className='logo me-auto'>
-        <a href='/council/main'>
-          <span>협의체공간</span>
-          <span></span>
-        </a>
-      </h1>
-      <nav id='navbar' className='navbar'>
-        <ul>
-          <li className='dropdown'><a href='/council/announce'>알려드려요</a>
-          </li>
-          <li className='dropdown'><a href='/council/oi'>오픈 이노베이션 레터</a>
-          </li>
-          <li className='dropdown'><a href='/council/archive'>스타트업 찾기</a>
-          </li>
-          <li><a href='/' className='getstarted'>INNOHI</a>
-          </li>
-          <li className='dropdown' ><a id='user_name' href='/'>로그인이 필요합니다.</a>
-            <ul>
-              <li id="LogOut">로그아웃</li>
-            </ul>
-          </li>
-        </ul>
-        <i className='bi bi-list mobile-nav-toggle'></i>
-      </nav>
-    </div>
-  </header>;
+  const headerCouncilElements =
+    //  <header id='header' className='fixed-top header-inner-pages' style={{display:'none'}}>
+    <header id='header' className='header-inner-pages' style={{ display: 'none' }}>
+      <div className='container d-flex align-items-center'>
+        <h1 className='logo me-auto'>
+          <a href='/council/main'>
+            <span>협의체공간</span>
+            <span></span>
+          </a>
+        </h1>
+        <nav id='navbar' className='navbar'>
+          <ul>
+            <li className='dropdown'><a href='/council/announce'>알려드려요</a>
+            </li>
+            <li className='dropdown'><a href='/council/oi'>오픈 이노베이션 레터</a>
+            </li>
+            <li className='dropdown'><a href='/council/archive'>스타트업 찾기</a>
+            </li>
+            <li><a href='/' className='getstarted'>INNOHI</a>
+            </li>
+            <li className='dropdown' ><a id='user_name' href='/'>로그인이 필요합니다.</a>
+              <ul>
+                <li id="LogOut">로그아웃</li>
+              </ul>
+            </li>
+          </ul>
+          <i className='bi bi-list mobile-nav-toggle'></i>
+        </nav>
+      </div>
+    </header>;
 
-
-
-class Header extends React.Component {
-
-  componentDidMount() {
+  useEffect(() => {
     GlobalFunction();
-  }
-  
-  render() {
-    if (window.location.pathname.split('/')[1] === 'council')
-      return (
-        <>
-          {headerCouncilElements}
-        </>
-      );
-    else
-      return (
-        <>
-          {headerElements}
-        </>
-      );
-  }
+  });
+
+  if (window.location.pathname.split('/')[1] === ('council'))
+    return (
+      <>
+        {headerCouncilElements}
+      </>
+    );
+  else if (window.location.pathname.split('/')[1] === ('test'))
+    return null
+  else
+    return (
+      <>
+        {headerElements}
+      </>
+    );
 
 }
 
