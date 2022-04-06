34ae4b49b456deba42951ea1a3d42c80d9d55ab7
inhyeeeok <nangmanvv@hanmail.net>
2022-04-06 15:51:38 +0900

    appsync 설정 파일 추가

---
+++ src/layouts/Client.js
@@ -0,0 +1,22 @@
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client';
+import { createAuthLink } from 'aws-appsync-auth-link';
+import AppSyncConfig from '../aws-exports';
+
+const url = AppSyncConfig.aws_appsync_graphqlEndpoint;
+const region = AppSyncConfig.aws_project_region;
+const auth = {
+    type: AppSyncConfig.aws_appsync_authenticationType,
+    apiKey: AppSyncConfig.aws_appsync_apiKey,
+};
+const link = ApolloLink.from([
+    // @ts-ignore
+    createAuthLink({ url, region, auth }),
+    // @ts-ignore
+    createHttpLink({ uri: url }),
+]);
+const Client = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+});
+
+export default Client;
