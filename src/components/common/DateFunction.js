6190dc990bb92fb401a70cbedd3bb54c09e00d8f
inhyeeeok <nangmanvv@hanmail.net>
2022-05-02 14:36:44 +0900

    스타트업 소싱 현황에서 사용할 StringToDateString 공통 함수 추가

--- src/components/common/DateFunction.js
+++ src/components/common/DateFunction.js
@@ -7,10 +7,12 @@ days = days >= 10 ? days : '0' + days;
 const hours = now.getHours();
 let minutes = now.getMinutes();
 minutes = minutes >= 10 ? minutes : '0' + minutes;
-const seconds = now.getSeconds();
+let seconds = now.getSeconds();
+seconds = seconds >=10 ? seconds : '0' + seconds;
 
 export const getToday = (shape) => {
-    console.log(shape)
+    console.log(shape);
+
     if (shape === 'yyyy') {
         return '' + years;
     } else if (shape === 'yyyymm') {
@@ -18,10 +20,15 @@ export const getToday = (shape) => {
     } else if (shape === 'yyyymmdd') {
         return '' + years + months + days;
     } else if (shape === 'yyyymmddhh') {
-        return '' + years + months + days + hours;
+        return '' + years + months + days + ''+hours;
     } else if (shape === 'yyyymmddhh24mi') {
-        return '' + years + months + days + hours + minutes;
+        return '' + years + months + days + ''+hours + ''+minutes;
     } else if (shape === 'yyyymmddhh24miss') {
-        return '' + years + months + days + hours + minutes + seconds;
+        return '' + years + months + days + ''+hours + ''+minutes + ''+seconds;
     }
+}
+
+export const StringToDateString = (param) => {
+    console.log(param);
+    return param.substr(0,4)+'년 '+ param.substr(4,2)+'월 ' + param.substr(6,2)+'일 ' + param.substr(8,2)+ '시 ' + param.substr(10,2)+ '분';
 }
