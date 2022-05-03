a9a1761d57ec20edf2fc924754af00b3822c3662
inhyeeeok <nangmanvv@hanmail.net>
2022-05-03 17:08:07 +0900

    이벤트 페이지 - 메인페이지 관련 최대 표출 갯수 5개, 단 조건에 맞는 이벤트가 부족할 경우 적게 표출되거나 아예 표출되지 않아도 됨
    조건 1. 현재 진행중인 이벤트
    조건 2. 추후 진행 예정인 이벤트(today()에서 가장 날짜가 가까운 이벤트 순)  #67

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -1,14 +1,122 @@
 import React, { useEffect } from 'react';
-
 import * as main from './MainElements';
-
 import SwiperComponents from '../../components/swipers/Swiper';
+import * as DateFunction from '../../components/common/DateFunction';
+import { useQuery, gql } from '@apollo/client';
 
 const Main = () => {
 
+  const selectTodo = gql`
+        query listInnohis {
+            listEventData {
+                items {
+                    endDate
+                    id
+                    img
+                    item
+                    link
+                    startDate
+                }
+        }
+        }
+  `;
+
+  const { loading, error, data } = useQuery(selectTodo);
+  console.log(error)
+  console.log(data)
+  console.log(loading)
+
+  const arrOrder = (key) => {
+    return (a, b) => {
+      if (a[key] > b[key]) {
+        return 1;
+      } else if (a[key] < b[key]) {
+        return -1;
+      }
+      return 0;
+    }
+  }
+
+  const ClassificationData = () => {
+    let ingData = [];
+    let comeData = [];
+    let finishData = [];
+
+    const today = DateFunction.getToday('yyyymmdd');
+
+    if (loading) {
+      return false;
+
+    } else {
+
+      data.listEventData.items.map((i) => {
+        if (today < i.startDate) {      //예정 today() < startdate 
+          return comeData.push(i);
+        } else if (i.endDate < today) { // 마감 endate < today()
+          return finishData.push(i);
+        } else {                        // 진행중 startdate <= today() <= enddate
+          return ingData.push(i);
+        }
+      })
+
+      ingData.sort(arrOrder('startDate'));
+      comeData.sort(arrOrder('startDate'));
+      finishData.sort(arrOrder('startDate'));
+      console.log(comeData);
+
+      const GridData = (param) => {
+        return param.dataSet.map(i => {
+          return (
+            <div key={i.item} className="swiper-slide"><a href={i.link} target="_blank" rel="noopener noreferrer"><img src={require(`../../assets/img/${i.img}`).default} className="img-fluid" style={{ width: "320px", height: "200px" }} alt=""></img></a></div>
+          )
+        })
+      }
+
+      const count = 5 - ingData.length;
+
+      if (count > 0) {
+        return (
+          <>
+            <GridData dataSet={ingData} />
+            <GridData dataSet={comeData.slice(0, count)} />
+          </>
+        )
+      } else {
+        return (
+          <GridData dataSet={ingData.slice(0, 5)} />
+        )
+      }
+    }
+
+  }
+
+
+  const ClientsElements = () => {
+    return (
+      <>
+        <section id="clients" className="clients">
+          <div className="container" data-aos="fade-up">
+            <header className="header-font">
+              <h4>지금, 이 행사 어때요?</h4>
+              <p>이노하이가 추천하는 놓쳐선 안 될 행사</p>
+            </header>
+
+            <div className="clients-slider swiper">
+              <div className="swiper-wrapper align-items-center">
+                <ClassificationData />
+              </div>
+              <div className="swiper-pagination"></div>
+            </div>
+
+          </div>
+        </section>
+      </>
+    )
+  }
+
   useEffect(() => {
     SwiperComponents();
-})
+  })
 
   return (
     <>
@@ -17,12 +125,12 @@ const Main = () => {
         {/* {main.cliensElements} */}
         {/* {main.refElements} */}
         {main.serviceElements}
-        {main.clientsElements}
+        <ClientsElements />
         {/*main.joinNewsletterElements*/}
       </main>
 
     </>
-  ); 
+  );
 
 }
 
