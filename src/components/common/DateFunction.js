056b87b4ca09635cfee2f6fb4c6754198b26dbcf
inhyeeeok <nangmanvv@hanmail.net>
2022-05-03 10:00:51 +0900

    hours 한자리 시간 오류 수정

--- src/components/common/DateFunction.js
+++ src/components/common/DateFunction.js
@@ -4,15 +4,15 @@ let months = new String(now.getMonth() + 1);
 months = months >= 10 ? months : '0' + months;
 let days =  new String(now.getDate());
 days = days >= 10 ? days : '0' + days;
-const hours = now.getHours();
+let hours = now.getHours();
+hours = hours >= 10 ? hours : '0' + hours;
 let minutes = now.getMinutes();
 minutes = minutes >= 10 ? minutes : '0' + minutes;
 let seconds = now.getSeconds();
 seconds = seconds >=10 ? seconds : '0' + seconds;
 
 export const getToday = (shape) => {
-    console.log(shape);
-
+    console.log(shape)
     if (shape === 'yyyy') {
         return '' + years;
     } else if (shape === 'yyyymm') {
@@ -29,6 +29,5 @@ export const getToday = (shape) => {
 }
 
 export const StringToDateString = (param) => {
-    console.log(param);
     return param.substr(0,4)+'년 '+ param.substr(4,2)+'월 ' + param.substr(6,2)+'일 ' + param.substr(8,2)+ '시 ' + param.substr(10,2)+ '분';
 }
