f734faef1d4541a0ab844bbfbfd2f040fff9b363
inhyeeeok <nangmanvv@hanmail.net>
2022-05-03 15:20:52 +0900

    #67 현재 이벤트 eventElement 파일 내에서 이벤트를 개별로 입력 / 수정 중 통합해 DB화 가능한지 검토 필요 -> 개발

--- src/views/insight/Contest.js
+++ src/views/insight/Contest.js
@@ -1,13 +1,52 @@
 import React, { useEffect } from 'react';
 import AOS from "aos";
 import Isotope from 'isotope-layout'
-
-
-import * as event from './EventElements';
+import { BiLink } from "react-icons/bi";
+import * as DateFunction from '../../components/common/DateFunction';
+import { useQuery, gql } from '@apollo/client';
 
 const Contest = () => {
 
-    window.addEventListener('load', () => {
+    const selectTodo = gql`
+        query listInnohis {
+            listEventData {
+                items {
+                    endDate
+                    id
+                    img
+                    item
+                    link
+                    startDate
+                }
+        }
+        }
+  `;
+
+    const eventFunction = () => {
+        const select = (el, all = false) => {
+            el = el.trim()
+            if (all) {
+                return [...document.querySelectorAll(el)]
+            } else {
+                return document.querySelector(el)
+            }
+        }
+
+        /**
+     * Easy event listener function
+     */
+        const on = (type, el, listener, all = false) => {
+            let selectEl = select(el, all)
+            if (selectEl) {
+                if (all) {
+                    selectEl.forEach(e => e.addEventListener(type, listener))
+                } else {
+                    selectEl.addEventListener(type, listener)
+                }
+            }
+        }
+
+
         let portfolioContainer = select('.portfolio-container');
         if (portfolioContainer) {
             let portfolioIsotope = new Isotope(portfolioContainer, {
@@ -32,39 +71,106 @@ const Contest = () => {
             }, true);
         }
 
-    });
+    }
 
-    const select = (el, all = false) => {
-        el = el.trim()
-        if (all) {
-            return [...document.querySelectorAll(el)]
+    setTimeout(() => { eventFunction() }, 100);
+
+    const { loading, error, data } = useQuery(selectTodo);
+    console.log(error)
+    console.log(data)
+    console.log(loading)
+
+    const ClassificationData = () => {
+        let ingData = [];
+        let comeData = [];
+        let finishData = [];
+
+        const today = DateFunction.getToday('yyyymmdd');
+
+        if (loading) {
+            return false;
         } else {
-            return document.querySelector(el)
+            data.listEventData.items.map((i) => {
+                if (today < i.startDate) {      //예정 today() < startdate 
+                    return comeData.push(i);
+                } else if (i.endDate < today) { // 마감 endate < today()
+                    return finishData.push(i);
+                } else {                        // 진행중 startdate <= today() <= enddate
+                    return ingData.push(i);
+                }
+            })
+
+            const GridF = (param) => {
+
+                return param.dataSet.map(i => {
+                    const betweenDate = `${i.startDate.substr(0, 4)}.${i.startDate.substr(4, 2)}.${i.startDate.substr(6, 2)} ~ ${i.endDate.substr(0, 4)}.${i.endDate.substr(4, 2)}.${i.endDate.substr(6, 2)}`
+
+                    return (
+                        <div className={param.classValue} key={i.item}>
+                            <a href={i.link}>
+                                <div className="portfolio-img"><img src={require(`../../assets/img/${i.img}`).default} className="img-fluid" alt=""></img></div>
+                                <div className="portfolio-info">
+                                    <h4>{i.item}</h4>
+                                    <p>{betweenDate}</p>
+                                    <a href={i.link} className="details-link" title="More Details"><BiLink /></a>
+                                </div>
+                            </a>
+                        </div>
+                    )
+                })
+            }
+
+            return (
+                <>
+                    <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                        <GridF dataSet={ingData} classValue='col-lg-4 col-md-6 portfolio-item filter-ing' />
+                        <GridF dataSet={comeData} classValue='col-lg-4 col-md-6 portfolio-item filter-come' />
+                        <GridF dataSet={finishData} classValue='col-lg-4 col-md-6 portfolio-item filter-finish' />
+                    </div>
+                </>
+            )
         }
+
+
     }
 
-    /**
- * Easy event listener function
- */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
+    const Elements = () => {
+        if (!loading) {
+            return (
+                <section id="portfolio" className="portfolio section-bg" style={{ marginTop: '61px' }}>
+                    <div className="container" data-aos="fade-up">
+
+                        <div className="section-title">
+                            <h2>Event</h2>
+                            <p>이런 이벤트들도 놓치지 마세요!</p>
+                        </div>
+
+                        <ul id="portfolio-flters" className="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+                            <li data-filter="*" className="filter-active">전체</li>
+                            <li data-filter=".filter-ing">진행중</li>
+                            <li data-filter=".filter-come">예정</li>
+                            <li data-filter=".filter-finish">마감</li>
+                        </ul>
+
+                        <ClassificationData />
+                    </div>
+                </section>
+            )
+        } else {
+            return (
+                <><p>데이터 다운로드 중...</p></>
+            )
         }
     }
-    
 
     useEffect(() => {
+
     });
 
     return (
         <>
             <main id="main">
-                {event.PortfolioElements}
+                <Elements />
             </main>
         </>
     );
