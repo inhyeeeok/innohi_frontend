0b373b91934dc0ae8c61f21dfff4102e7587ec67
inhyeeeok <nangmanvv@hanmail.net>
2022-05-16 15:49:03 +0900

    #74 개발 반영 close

--- src/views/council/CouncilCarousel.js
+++ src/views/council/CouncilCarousel.js
@@ -1,6 +1,8 @@
 import { useQuery, gql } from '@apollo/client';
+import SwiperComponents from '../../components/swipers/Swiper';
+import React, { useEffect } from 'react';
 
-const CouncilCarousel = () => {
+const CouncilCarousel = (param) => {
 
     const selectTodo = gql`
     query listInnohis {
@@ -26,16 +28,33 @@ const CouncilCarousel = () => {
                     title
                 }
         }
-        }
+
+        listStartupArchiveData (limit: 500) {
+            items {
+              id
+              category_L
+              city
+              category_S
+              country
+              employeeNum
+              estbYear
+              investRound
+              logoLink
+              moneyRaised
+              name
+              service
+              no
+            }
+          }
+    }
   `;
 
     const { loading, data } = useQuery(selectTodo);
-    // console.log(loading, data);
 
     const CarouselMainElements = () => {
-        
+
         const listNewsletterData = ((data.listNewsletterData.items)?.slice(0)).sort(function (a, b) { return b.bno - a.bno }).slice(0, 2);
-        const listNoticeData = ((data.listNoticeData.items)?.slice(0)).sort(function (a, b) { return b.bno - a.bno }).slice(0,2);
+        const listNoticeData = ((data.listNoticeData.items)?.slice(0)).sort(function (a, b) { return b.bno - a.bno }).slice(0, 2);
 
         return (
             <>
@@ -47,7 +66,7 @@ const CouncilCarousel = () => {
                                 <div style={{ background: '#FFFFFF', marginTop: '10px' }}>
                                     <div className='row'>
                                         <div className="col-xl-12 col-md-12 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                                            <a href={'/council/announce/detail/'+ Number(listNoticeData[0].bno)}>
+                                            <a href={'/council/announce/detail/' + Number(listNoticeData[0].bno)}>
                                                 <div className="icon-box" style={{ padding: '10px' }}>
                                                     <img src={require('../../assets/img/council/announce/' + Number(listNoticeData[0].bno) + '/' + listNoticeData[0].content).default} className="img-fluid" style={{ width: '50%' }} alt=""></img>
                                                     <p style={{ color: '#000000' }}>{listNoticeData[0].title}</p>
@@ -60,7 +79,7 @@ const CouncilCarousel = () => {
                                     <div style={{ background: '#FFFFFF', marginBottom: '10px' }}>
                                         <div className='row'>
                                             <div className="col-xl-12 col-md-12 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                                                <a href={'/council/announce/detail/'+ Number(listNoticeData[1].bno)}>
+                                                <a href={'/council/announce/detail/' + Number(listNoticeData[1].bno)}>
                                                     <div className="icon-box" style={{ padding: '10px' }}>
                                                         <img src={require('../../assets/img/council/announce/' + Number(listNoticeData[1].bno) + '/' + listNoticeData[1].content).default} className="img-fluid" style={{ width: '50%' }} alt=""></img>
                                                         <p style={{ color: '#000000' }}>{listNoticeData[1].title}</p>
@@ -78,7 +97,7 @@ const CouncilCarousel = () => {
                                     <div style={{ background: '#FFFFFF', marginTop: '10px' }}>
                                         <div className='row'>
                                             <div className="col-xl-12 col-md-12 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                                                <a href={'/council/oi/detail/'+ Number(listNewsletterData[0].bno)}>
+                                                <a href={'/council/oi/detail/' + Number(listNewsletterData[0].bno)}>
                                                     <div className="icon-box" style={{ padding: '10px' }}>
                                                         <img src={require('../../assets/img/council/oi/newsletter_main_page.png').default} className="img-fluid" style={{ width: '50%' }} alt=""></img>
                                                         <p style={{ color: '#000000' }}>{listNewsletterData[0].title}</p>
@@ -90,7 +109,7 @@ const CouncilCarousel = () => {
                                     <div style={{ background: '#FFFFFF', marginBottom: '10px' }}>
                                         <div className='row'>
                                             <div className="col-xl-12 col-md-12 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                                                <a href={'/council/oi/detail/'+ Number(listNewsletterData[1].bno)}>
+                                                <a href={'/council/oi/detail/' + Number(listNewsletterData[1].bno)}>
                                                     <div className="icon-box" style={{ padding: '10px' }}>
                                                         <img src={require('../../assets/img/council/oi/newsletter_main_page.png').default} className="img-fluid" style={{ width: '50%' }} alt=""></img>
                                                         <p style={{ color: '#000000' }}>{listNewsletterData[1].title}</p>
@@ -108,15 +127,56 @@ const CouncilCarousel = () => {
         )
     }
 
-    if(loading) {
-        return (<><p></p></>)
-    }else {
+    const CarouselStartupElements = () => {
+
+        const listStartupArchiveData = ((data.listStartupArchiveData.items)?.slice(0)).sort(function (a, b) { return b.no - a.no }).slice(0, 5);
+
+        const ListRendering = () => {
+            return (
+                listStartupArchiveData.map((i) => {
+                    return (
+                        <div className="swiper-slide" key={i.no}><a href={'../../council/archive/detail/' + i.no}><img src={i.logoLink} className="img-fluid" style={{ width: "320px", height: "200px" }} alt=""></img></a></div>
+                    )
+                })
+            )
+        }
+
         return (
-            <CarouselMainElements />
+            <section id="clients" className="clients" style={{ background: '#f3f5fa' }}>
+                <div className="container" data-aos="fade-up">
+                    <header className="header-font">
+                        <h2><a href='/council/archive'><span style={{ color: '#37517e' }}>이런 스타트업은 어때요?</span>    <span style={{ fontSize: '24px' }}>+</span></a></h2>
+                    </header>
+
+                    <div className="clients-slider swiper">
+                        <div className="swiper-wrapper align-items-center">
+                            <ListRendering />
+                        </div>
+                        <div className="swiper-pagination"></div>
+                    </div>
+
+                </div>
+            </section>
         )
     }
-  
 
+    useEffect(() => {
+        SwiperComponents();
+      })
+
+    if (loading) {
+        return (<><p></p></>)
+    } else {
+        if (param.type === 'operation') {
+            return (
+                <CarouselMainElements />
+            )
+        } else {
+            return (
+                <CarouselStartupElements />
+            )
+        }
+    }
 
 }
 
--- src/views/council/CouncilMain.js
+++ src/views/council/CouncilMain.js
@@ -5,14 +5,10 @@ import { withAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 import config from '../../aws-exports';
 
-import * as council from './CouncilMainElements'
 import * as CouncilCommon from './CouncilCommon'
-import SwiperComponents from '../../components/swipers/Swiper';
-
+import CouncilCarousel from '../council/CouncilCarousel'
 import Modal from '../../components/modal/Modal'
 import * as jsondata from '../../components/JsonData'
-
-import CouncilCarousel from '../council/CouncilCarousel'
 
 Amplify.configure(config)
 
@@ -81,9 +77,25 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
     }
   }
 
+  const HeroElements = () => {
+    return (
+      <section id='hero' className='d-flex align-items-center' >
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1' data-aos="fade-up" data-aos-delay="200">
+            <h1><span style={{ color: 'rgb(253, 142, 77)' }}>INNOHI </span>협의체</h1>
+            <h2>오픈이노베이션 레터와 스타트업 찾기 등 <br />INNOHI의 여러가지 소식들을 이곳에서 확인해보세요</h2>
+          </div>
+          <div className='col-lg-6 order-1 order-lg-2 hero-img' data-aos="zoom-in" data-aos-delay="200">
+            <img src={require('../../assets/img/intro/histroy/travel.jpg').default} className='img-fluid animated' alt=''></img>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+  }
 
   useEffect(() => {
-    SwiperComponents();
     popUpCheck();
     affiliateCheck(user);
     CouncilCommon.eventLogOut(signOut);
@@ -131,9 +143,9 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
   return (
     <>
       <ModalElements />
-      {council.heroElements}
-      <CouncilCarousel />
-      {council.clientsElements}
+      <HeroElements />
+      <CouncilCarousel type='operation'/>
+      <CouncilCarousel type='startup'/>
     </>
   )
 
@@ -141,15 +153,12 @@ const CouncilMain = ({ isPassedToWithAuthenticator, signOut, user }) => {
 }
 
 export default withAuthenticator(CouncilMain, {
-  includeGreetings: true,
-  socialProviders: ['google'],
-  hideSignUp: [true],
-  //   loginMechanisms : ['username'],
-  loginMechanisms: ['email'],
-  //  components : [components],
-  variation: ["modal"]
-}, false, [
-  //  <MySignIn /> 
+    includeGreetings: true,
+    socialProviders: ['google'],
+    hideSignUp: [true],
+    loginMechanisms: ['email'],
+    variation: ["modal"]
+  }, false, [
 ]);
 
 
