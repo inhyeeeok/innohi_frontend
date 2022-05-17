2a27bd9a472f7f33cbe01a3e9dffbfff18e966cb
inhyeeeok <nangmanvv@hanmail.net>
2022-05-17 16:03:16 +0900

    #72 불필요한 import 삭제

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'react-app-polyfill/stable';
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter} from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 
 import Public from './layouts/Public';
 
@@ -15,8 +14,6 @@ import './assets/style/custom.css';
 
 import 'bootstrap/dist/js/bootstrap.js'
 import 'jquery/dist/jquery.min.js';
-import "datatables.net-dt/js/dataTables.dataTables";
-import "datatables.net-dt/css/jquery.dataTables.min.css";
 
 import ReactGA from 'react-ga';
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 발급받은 추적ID를 환경 변수로 불러온다.
@@ -25,15 +22,10 @@ ReactGA.initialize(TRACKING_ID);
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-
       <Public />
-   
     </React.StrictMode>
   </BrowserRouter>,
   
   document.getElementById("root")
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
