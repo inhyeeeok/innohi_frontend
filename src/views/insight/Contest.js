f9f37d9cb53cb01bd6ed0917b2f06deb1a80d562
inhyeeeok <92982812+inhyeeeok@users.noreply.github.com>
2022-07-26 16:34:04 +0900

    Update Contest.js
    #78 closed

--- src/views/insight/Contest.js
+++ src/views/insight/Contest.js
@@ -82,9 +82,9 @@ const Contest = () => {
 
     const arrOrder = (key) => {
         return (a, b) => {
-            if (a[key] > b[key]) {
+            if (a[key] < b[key]) {
                 return 1;
-            } else if (a[key] < b[key]) {
+            } else if (a[key] > b[key]) {
                 return -1;
             }
             return 0;
@@ -113,9 +113,13 @@ const Contest = () => {
                 }
             })
 
+            
+
             ingData.sort(arrOrder('startDate'));
             comeData.sort(arrOrder('startDate'));
             finishData.sort(arrOrder('startDate'));
+
+            console.log(finishData);
 
             const GridF = (param) => {
 
